6ff528695d9b259c08f780c45d4fd4fb7d306095
Fabian Ström <fs@fabianstrom.se>
2014-08-09 17:14:18 +0200

    moved to api dir, added absolute path

---
+++ api/config.php
@@ -0,0 +1,32 @@
+<?php
+
+/*
+ * The working directory for latexmk.
+ * The http user should have read/write access to thiw directory.
+ * The path should not end with a slash.
+ * The path should be absolute.
+ */
+define("WORKDIR", "");
+
+/*
+ * The ip address and port of etherpad-lite.
+ * Should be on the form http(s)://127.0.0.1:9001
+ */
+define("ETHERPADLITEHOST", "");
+
+/*
+ * The api key defined in etherpad-lite's settings.json
+ */
+define("ETHERPADLITEAPIKEY", "");
+
+/*
+ * The absolute path to the latexmk binary
+ */
+define("LATEXMKPATH", "");
+
+/*
+ * The absolute path to the pdflatex binary
+ */
+define("PDFLATEXPATH", "");
+
+?>
