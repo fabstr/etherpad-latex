5489d138a20fb88f792bffe879a58dcd447557d8
Fabian Ström <fs@fabianstrom.se>
2014-08-26 16:22:55 +0200

    renamed managegroups.html to groups.html

--- js/app.js
+++ js/app.js
@@ -14,7 +14,7 @@ config(['$routeProvider', function($routeProvider) {
   $routeProvider.when('/login', {templateUrl: 'partials/login.html', controller: 'LoginController'});
   $routeProvider.when('/logout', {templateUrl: 'partials/logout.html', controller: 'LogoutController'});
   $routeProvider.when('/edit/:group/:name', {templateUrl: 'partials/edit.html', controller: 'EditController'});
-  $routeProvider.when('/groups/', {templateUrl: 'partials/managegroups.html', controller: 'ManageGroupsController'});
+  $routeProvider.when('/groups/', {templateUrl: 'partials/groups.html', controller: 'ManageGroupsController'});
   $routeProvider.when('/groupdetails/:groupid/', {templateUrl: 'partials/groupdetails.html', controller: 'GroupDetailsController'});
   $routeProvider.otherwise({redirectTo: '/login'});
 }]);
